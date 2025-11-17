54d2ac93956787f79678519e6911a768e3b922ea
gibson <gibsonjoseph890@gmail.com>
2025-11-17 20:41:39 +0530

    learn string calculation and extractions

--- 7_rowLevelFunctions/1_stringFunctions/2_calculation.sql
+++ 7_rowLevelFunctions/1_stringFunctions/2_calculation.sql
@@ -1,4 +1,14 @@
 -- STRING CALCULATION
 /*
- 1. LEN
+ LENGTH
+ 
+ The LENGTH() function is used to calculate the number of characters in a string.
+ It returns a numeric value.
+ Counts how many characters.
  */
+-- ======================================
+-- Calculate the length of each customer's first name
+-- ======================================
+SELECT first_name,
+    LENGTH(first_name) AS len_name
+FROM customers;
