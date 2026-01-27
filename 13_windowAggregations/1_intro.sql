f490236ad1818d9105ac870c6fd0bd133e678833
gibson <gibsonjoseph890@gmail.com>
2026-01-27 19:11:09 +0530

    Added the documents

---
+++ 13_windowAggregations/1_intro.sql
@@ -0,0 +1,4 @@
+-- Window Aggretation function
+/*
+ If you apply and aggregate function in SQL what can happen, SQL can go through all rows of the window or the entire data, and start aggregating the data. So that means in the result in the output, SQL is going to give you one single aggregated value.
+ */
