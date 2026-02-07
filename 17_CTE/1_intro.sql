022a5a3f36932f1339677edcef147ea16ba1e95d
gibson <gibsonjoseph890@gmail.com>
2026-02-07 14:18:46 +0530

    Updated CTE introduction

--- 17_CTE/1_intro.sql
+++ 17_CTE/1_intro.sql
@@ -10,6 +10,10 @@
  
  What is the different between Subquery and CTE?
  - The story is identical between the subqueries and the CTE. 
+ - The subqueries are writing it from bottom to top. So first we have this inner query the subquery and then on top of it we have the main query.
+ - But now, on the other hand, the CTE, we are writing it from top to bottom. So first we write this inner query the CTE query, and then beneath it we're going to go and write the main query.
+ - So if we thinkg about the subqueries we start from bottom to top. If we thinking about CTE we start from top to bottom.
+ - The subquery is introducing and intermediate result that is used later from teh main query, And the same thing for the CTE it presentes like intermediate table that is used as well from the main query.
  - In the subquery the result can be used only once. So you cannot have another place in your main query where you go and reuse the results from the subquery. So you can use it maximum only in one position and only once.
  - Whet the CTE technique you can think about the the sales table as a virtual table, and not only you can use it in one place in the main query, you can go and use it in many other places.
  
