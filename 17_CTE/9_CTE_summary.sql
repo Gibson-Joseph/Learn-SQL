2599b09b3db57019062110da82313d43595be7bc
gibson <gibsonjoseph890@gmail.com>
2026-02-07 19:25:07 +0530

    CTE Summary

---
+++ 17_CTE/9_CTE_summary.sql
@@ -0,0 +1,17 @@
+-- CTE Summary
+/*
+ CTE the common table expression is a temporary named result, like a virtual that could be used from different places in the query. 
+ 
+ Advantages:
+ 1. Readabilty:
+ -- It breaks the complexity of query into small multiple pieces which makes our query much easier to read and as well to understand, so it improves readability.
+ 2. Modularity:
+ -- Those small multiple pieces, they are really easy to manage and develop. So those pieces are like self-contained which makes our queries more modular. So it introudces modularity inside our queries.
+ 3. Reusability:
+ -- CTE help us to reduce the redundancy inside our queries, where it makes the results of one query usable in multiple places inside our query. So it makes our code smaller and reduce redundancy.
+ 4. Recursion:
+ -- CTE help us to do lopping and iterating in SQL by using the recursive CTE.
+ 
+ 
+ TIP: Don't create more than 5 CTEs in one query.
+ */
