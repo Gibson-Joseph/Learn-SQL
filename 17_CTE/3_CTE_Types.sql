e9b8ec2dc6e4f4270693a95715fda6ef8feb768a
gibson <gibsonjoseph890@gmail.com>
2026-02-07 14:39:33 +0530

    CTE Types

---
+++ 17_CTE/3_CTE_Types.sql
@@ -0,0 +1,11 @@
+-- CTE Types
+/*
+ We have two types of CTE:
+ 
+ 1. Non-Recursive CTE
+ - in non-recursive CTE we have two subtypes.
+ -- 1. Stantalone CTE
+ -- 2. Nested CTE
+ 
+ 2. Recursive CTE 
+ */
