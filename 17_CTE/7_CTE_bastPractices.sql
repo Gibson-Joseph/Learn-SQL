71e890950a84bfb167600276db2f69c3c9c125da
gibson <gibsonjoseph890@gmail.com>
2026-02-07 17:11:22 +0530

    CTE best practices

---
+++ 17_CTE/7_CTE_bastPractices.sql
@@ -0,0 +1,7 @@
+-- CTE Best Practies
+/*
+ 1. It is really alwasy important to rethink and refactor your CTE in ourder to merge it into one and reduce the number of CTE.
+ 2. Don't use more than 5 CTEs in one query; otherwise, your code will be hard to understand and maintain.
+
+ Try to not overuse the CTEs in your projects, not for each step.
+ */
