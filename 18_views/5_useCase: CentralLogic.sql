ed828a7ad3d3fda9248b1543a487b2eecc718d0e
gibson <gibsonjoseph890@gmail.com>
2026-02-07 21:02:23 +0530

    Use Case: Central logic

---
+++ 18_views/5_useCase: CentralLogic.sql
@@ -0,0 +1,6 @@
+-- Use Case: Central Logic
+-- Ref: https://www.udemy.com/course/the-complete-sql-bootcamp-30-hours-go-from-zero-to-hero/learn/lecture/45927937#overview
+/*
+ 1. Central Query Logic:
+ -- Store central logic from a complex query in the database so that everyone can access it. And with that, we improve reusablity between multiple queries, and we reduce as well the complexity of the overall projects.
+ */
