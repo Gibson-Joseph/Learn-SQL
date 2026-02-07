299b917d3125110a78b2d56c56a9414b99ed467d
gibson <gibsonjoseph890@gmail.com>
2026-02-07 20:53:12 +0530

    Views Vs Tables

---
+++ 18_views/4_viewsVsTables.sql
@@ -0,0 +1,15 @@
+-- Views Vs Tables
+/*
+ Tables: 
+ -- Tables stores the actuall data physically at a database. So the tables where the data is persisted.
+ -- The tables are hard to maintain and as well hard to change. So it needs a lot of efforts in order to do any change like adding columns.
+ -- Tables re faster than Views.
+ -- You can read and write tables.
+ 
+ Views:
+ --  Views they are virtual tables, and they do not store any data inside the database, but they present the data from the underlying tables. So that means views don't persist any data physically
+ -- Views are way easier to maintain and very flexible to change. All that you have to do is only to change the query of the view. So that means you can very quickly change stuff in the views compared to the tables.
+ -- Slow response.
+ -- Read only.
+ 
+ */
