a01de09742592fa941b16e6273a31733582051d3
gibson <gibsonjoseph890@gmail.com>
2026-02-10 20:08:43 +0530

    Views Vs CTE

---
+++ 18_views/6_viewsVsCTE.sql
@@ -0,0 +1,12 @@
+-- Views Vs CTE
+/*
+ CTE:
+ -- The CTE is used in order to reduce the redundancy within one single query. So it improves the reusability within one query. 
+ -- Improve reusability in one queries
+ -- Temporary Logic
+ 
+ VIEWS:
+ -- We are reducing the redundancies from the multiple queries. So we are reducing the complexity of the whole project.
+ -- Improve reusability in multi queries
+ -- Persisted logic
+ */
